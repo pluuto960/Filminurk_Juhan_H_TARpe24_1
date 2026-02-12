0bb10519e8ce451c1b51945c87ecc79fd172229a
pluuto960 <189347604+pluuto960@users.noreply.github.com>
2026-02-12 21:25:08 +0200

    Started on Chatroom

---
+++ Filminurk/Filminurk/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Filminurk.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
