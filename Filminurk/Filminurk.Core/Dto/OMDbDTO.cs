32c92d8e877bda74e06f38163aa49e007b29dcdc
pluuto960 <189347604+pluuto960@users.noreply.github.com>
2026-02-24 14:14:19 +0200

    Start of OMdb, Öisasin OMDbDTO väljad

--- Filminurk/Filminurk.Core/Dto/OMDbDTO.cs
+++ Filminurk/Filminurk.Core/Dto/OMDbDTO.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Filminurk.Core.Dto
 {
-    internal class OMDbDTO
+    public class OMDbDTO
     {
+        public string? Title { get; set; }
+        public string? Year { get; set; }
+        public string? Plot { get; set; }
+        public string? Director { get; set; }
+        public string? Actors { get; set; }       // "Actor1, Actor2, Actor3"
+        public string? Genre { get; set; }         // "Action, Drama"
+        public string? imdbRating { get; set; }    // "9.0"
+        public string? Released { get; set; }      // "18 Jul 2008"
+        public string? Response { get; set; }      // "True" / "False"
+        public string? Error { get; set; }
     }
 }
